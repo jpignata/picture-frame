520dfd7839f66a01c6af548911152d9bb45d2818
John Pignata <john@pignata.com>
2012-10-28 21:47:58 -0400

    Add ping to keep Heroku connection open

--- lib/actions/subscribe.rb
+++ lib/actions/subscribe.rb
@@ -6,11 +6,8 @@ module Actions
     end
 
     def run
-      @channel.subscribe { |message| send_picture_event(message) }
-
-      if @channel.last_message
-        EventMachine.add_timer(0.1) { send_picture_event(@channel.last_message) }
-      end
+      schedule_ping
+      subscribe_to_channel
 
       [200, headers, body]
     end
@@ -25,6 +22,23 @@ module Actions
     def body
       @body ||= Body.new
     end
+
+    def schedule_ping
+      timer = EventMachine::PeriodicTimer.new(15) do
+        body.write(": ping\n\n")
+        body.callback { timer.cancel }
+        body.errback { timer.cancel }
+      end
+    end
+
+    def subscribe_to_channel
+      @channel.subscribe { |message| send_picture_event(message) }
+
+      if @channel.last_message
+        EventMachine.add_timer(0.1) { send_picture_event(@channel.last_message) }
+      end
+    end
+
 
     def headers
       {
